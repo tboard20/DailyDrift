759b0eb74520e94e653b32b827b473d380cc1260
tboard20 <tboad04@gmail.com>
2025-07-21 13:57:05 +0100

    commit2

--- dailyblog/dilyapp/models.py
+++ dailyblog/dilyapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+       return self.name
+        
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to='articles/')
+    pub_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
