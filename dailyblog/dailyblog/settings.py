d519b376f09faf173cbcfa61d94c5185908561f6
Olakunle Oluwatobiloba <tboad04@gmail.com>
2025-09-23 13:07:32 +0100

    commit25

--- dailyblog/dailyblog/settings.py
+++ dailyblog/dailyblog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY", default="django-insecure-change-me")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = config("DEBUG", default=True, cast=bool)
 
 # Allow Render host + localhost
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
